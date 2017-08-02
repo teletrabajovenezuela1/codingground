9e7177742ba7b675a4e00944edcaf1463b318804
teletrabajovenezuela1 <teletrabajovenezuela1@gmail.com>
2017-08-01 21:30:16 -0400

    Created by Coding Ground Client

---
+++ New Project-20170801/funciones.h
@@ -0,0 +1,22 @@
+namespace atk
+{
+    void saludar();
+    void despedir();
+}
+
+//orden de trabajo namespace
+namespace ot
+{
+    //datos generales
+    namespace gen
+    {
+        void getId();
+        void getFecha();
+    }
+    
+    //datos cliente
+    namespace cli
+    {
+        void getNombre();
+    }
+}
