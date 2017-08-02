ccf018ccbf4b55f9fb06e164ef3c0c801303dfd7
teletrabajovenezuela1 <teletrabajovenezuela1@gmail.com>
2017-08-02 15:27:08 -0400

    Created by Coding Ground Client

---
+++ New Project-20170801/OrdenDeTrabajo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "OrdenDeTrabajo.h" 
+
+using namespace std;
+using namespace ot;
+
+//constructor
+OrdenDeTrabajo::OrdenDeTrabajo(){}
+
+
+void OrdenDeTrabajo::getId()
+{
+    cout << "Id" << endl;
+}
+
+
+void OrdenDeTrabajo::getFecha()
+{
+    cout << "Fecha" << endl;
+}
+
+
+void OrdenDeTrabajo::getNombre()
+{
+    cout << "Nombre" << endl;
+}
+
+
+void OrdenDeTrabajo::getDatos()
+{
+    cout << "Leer datos" << endl;
+}
